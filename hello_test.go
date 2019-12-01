025f3058ceee843232b8c5c556f4eaa8dcdc462a
Steffen Krause <steffen.krause@soabridge.com>
2019-12-01 00:35:20 -0500

    Squashed some more bugs

--- hello_test.go
+++ hello_test.go
@@ -8,28 +8,27 @@ import (
 func TestHelloEmpty(t *testing.T) {
 	want := "Hello, world!"
 	if got, _ := Hello(""); got != want {
-		t.Errorf(fmt.Sprintf("Hello(\"\")=%q; want %q", got, want))
+		t.Errorf(fmt.Sprintf("Hello(\"\")=%q,true; want %q,true", got, want))
 	}
 }
 
 func TestHelloInvalid(t *testing.T) {
-	want := "Hello, world!"
-	if got, _ := Hello(23); got != want {
-		t.Errorf(fmt.Sprintf("Hello(\"\")=%q; want %q", got, want))
+	if got, ok := Hello(23); ok {
+		t.Errorf(fmt.Sprintf("Hello(23)=%q,%t; want \"\",false", got, ok))
 	}
 }
 
 func TestHelloNil(t *testing.T) {
 	want := "Hello, world!"
 	if got, _ := Hello(nil); got != want {
-		t.Errorf(fmt.Sprintf("Hello(\"\")=%q; want %q", got, want))
+		t.Errorf(fmt.Sprintf("Hello(nil)=%q; want %q", got, want))
 	}
 }
 
 func TestHelloWorld(t *testing.T) {
 	want := "Hello, world!"
 	if got := HelloWorld(); got != want {
-		t.Errorf(fmt.Sprintf("Hello(\"\")=%q; want %q", got, want))
+		t.Errorf(fmt.Sprintf("HelloWorld()=%q; want %q", got, want))
 	}
 }
 
